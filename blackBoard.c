008d4f509dd4b78790dcd81732f85b31c4665fb0
Mattia <mattia.tinfena@gmail.com>
2024-11-02 11:06:12 +0100

    blackboard first fork test

--- blackBoard.c
+++ blackBoard.c
@@ -1,11 +1,11 @@
 #include <ncurses.h>
-
 #include <stdio.h>
 #include <string.h>
 #include <fcntl.h>  
 #include <stdlib.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <sys/stat.h>
 #include <sys/wait.h>
 
 #define w1 0        
@@ -19,11 +19,14 @@
 #define read 0
 #define write 1 
 
+#define PROCESSNUM 4
+#define PIPESNUM 4
+
 int main() {
 
-    int pipes [4][2][2];
+    int pipes [PIPESNUM][2][2];
 
-    for (int i = 0; i < 4; i++) {
+    for (int i = 0; i < PIPESNUM; i++) {
         for (int j = 0; j < 2; j++){
             if (pipe(pipes[i][j]) == -1) {
                 perror("Pipe creation error");
@@ -32,9 +35,21 @@ int main() {
         }
     }
 
-    pid_t pid1, pid2, pid3, pid4;
+    pid_t pids [PROCESSNUM];
+    char fd_str[15];
 
-    
+    sprintf(fd_str, "%d", pipes[w1][ask][read]);
+    char *args[] = {fd_str};
+
+    // for (int i = 0; i < PROCESSNUM; i++){
+        pids[1] = fork();
+        if(pids[1] != 0){
+            return pids[1];
+        }
+        else{
+            execl("./w1", "./w1", fd_str, NULL);
+        }
+    // }
 
     wait(NULL);
     wait(NULL);
