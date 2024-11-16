ffb488bfcf5ca518b2443af6caf1cf8be73b6f69
ChiaraBuono <7687956@studenti.unige.it>
2024-11-16 16:37:20 +0000

    Moving drone without weighted forces

--- drone.c
+++ drone.c
@@ -8,6 +8,7 @@
 #include <sys/stat.h>
 #include <sys/wait.h>
 #include "auxfunc.h"
+#include <math.h>
 
 
 // process that ask or receive
@@ -15,6 +16,88 @@
 #define askrd 0
 #define recwr 3
 #define recrd 2
+
+#define MAX_DIRECTIONS 80
+#define WINDOW_WIDTH 400
+#define WINDOW_LENGHT 800
+
+typedef struct {
+    float x;
+    float y;
+
+} Element;
+
+typedef struct {
+    int x;
+    int y;
+} Element_bb;
+
+const char* moves[] = {"up", "down", "right", "left", "upleft", "upright", "downleft", "downright"};
+Element drone = {10.0, 20.0};  
+
+// Useful for debugging
+void printPosition(Element p) {
+    printf("Position: (x = %f, y = %f)\n", p.x, p.y);
+    printf("BB Pos: (x = %.0f, y = %.0f)\n", round(p.x), round(p.y));
+}
+
+// Simulate user input
+void fillDirections(const char* filled[], int* size) {
+    for (int i = 0; i < *size; i++) {
+        int randomIndex = rand() % 8;
+        filled[i] = moves[randomIndex]; 
+    }
+}
+
+// Update drone position
+void updatePosition(Element* p, const char* direction) {
+    int up_condition = (p->y > 1) ? 1 : 0;
+    int down_condition = (p->y < WINDOW_WIDTH - 1) ? 1 : 0;
+    int right_condition = (p->x < WINDOW_LENGHT - 1) ? 1 : 0;
+    int left_condition = (p->x > 1) ? 1 : 0;
+
+
+    if (strcmp(direction, "up") == 0 && up_condition) p->y -= 1;
+    else if (strcmp(direction, "down") == 0 && down_condition) p->y += 1;
+    else if (strcmp(direction, "right") == 0 && right_condition) p->x += 1;
+    else if (strcmp(direction, "left") == 0 && left_condition) p->x -= 1;
+
+    else if (strcmp(direction, "upleft") == 0 && up_condition && left_condition) {
+        // printf("Before -");
+        // printPosition(drone);
+        p->x -= 1.0 / sqrt(2);
+        p->y -= 1.0 / sqrt(2);
+        // printf("Upleft - ");
+        // printPosition(drone);
+        }
+    else if (strcmp(direction, "upright") == 0 && up_condition && right_condition) {
+        p->x += 1.0 / sqrt(2);
+        p->y -= 1.0 / sqrt(2);
+    }
+    else if (strcmp(direction, "downleft") == 0 && down_condition && left_condition) {
+        p->x -= 1.0 / sqrt(2);
+        p->y += 1.0 / sqrt(2);
+    }
+    else if (strcmp(direction, "downright") == 0 && down_condition && right_condition) {
+        p->x += 1.0 / sqrt(2);
+        p->y += 1.0 / sqrt(2);
+    }
+
+}
+
+// Remove first element of the array
+void removeFirstElement(const char* directions[], int* size) {
+    if (*size == 0) {
+        *size = MAX_DIRECTIONS;
+        printPosition(drone);
+        fillDirections(directions, size);  // Refill the array if empty
+    } else {
+        for (int i = 1; i < *size; ++i) {
+            directions[i - 1] = directions[i]; 
+        }
+        (*size)--;
+    }
+}
 
 int main(int argc, char *argv[]) {
     if (argc < 2) {
@@ -57,10 +140,23 @@ int main(int argc, char *argv[]) {
     close(fds[askrd]);
     close(fds[recwr]);
 
+    const char* directions[MAX_DIRECTIONS];
+    int directionCount = MAX_DIRECTIONS;
+
+    // Simulate user input
+    fillDirections(directions, &directionCount);
+    //for (int i = 0; i < MAX_DIRECTIONS; i++) printf("%s\n", directions[i]);
+
 
     while (1) {
+        updatePosition(&drone, directions[0]);
+        if (drone.x < 0 || drone.y < 0 || drone.x > WINDOW_LENGHT || drone.y > WINDOW_WIDTH) printf("ERROR");
 
-       ;
+        //printf("Removing direction: %s\n", directions[0]);
+        removeFirstElement(directions, &directionCount);
+
+
+        usleep(100000); 
     }
     
     // Chiudiamo il file
